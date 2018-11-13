9aa18e3aa42e54c40e4e2d50f89b4fa439d60dfc
Franklin Bitencourt <Chingling152@hotmail.com>
2018-11-13 21:06:40 -0200

    V 0.2 + Adicionado TransacaoController.Mostrar (Não testado ainda) + Adicionado Tabela em Mostrar.cshtml (Sem dados)

--- Controllers/TransacaoController.cs
+++ Controllers/TransacaoController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.IO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -28,6 +29,35 @@ namespace Projeto.Financas.Transacoes.Controllers
                 sw.WriteLine($"{tra.NumeroTransacao};{tra.Descricao};{tra.tipo};{tra.valor};{tra.data}");
             }
             ViewBag.Mensagem = $"Transação {tra.NumeroTransacao} cadastrada com sucesso!";
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Mostrar(){
+            if(string.IsNullOrEmpty(HttpContext.Session.GetString("emailUsuario"))){
+                return RedirectToAction("Login","Usuario");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Mostrar(IFormCollection formulario){
+            List<Transacao> transacoes = new List<Transacao>();
+            StreamReader reader = new StreamReader("Transacao.csv",true);
+
+            List<string> nomes = new List<string>();
+
+            while(!reader.EndOfStream){             
+                string[] info = reader.ReadLine().Split(';');
+                Transacao t = new Transacao(){
+                    NumeroTransacao = int.Parse(info[0]),
+                    Descricao = info[1],
+                    tipo = info[2],
+                    valor = double.Parse(info[3]),
+                    data = System.DateTime.Parse(info[4])
+                };
+                transacoes.Add(t);
+            }
             return View();
         }
     }
